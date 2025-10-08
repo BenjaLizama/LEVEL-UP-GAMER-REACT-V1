b7693b23e9d457cac1eb6e227dcbd869dbf5fb22
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-08 02:49:31 -0300

    feat(utils): Agrega carpeta utils e implementa archivos icons.js y color.js

---
+++ src/assets/utils/icons.js
@@ -0,0 +1,32 @@
+import {
+  faGamepad as faGamepadSolid,
+  faHeart as faHeartSolid,
+  faGift as faGiftSolid,
+  faWallet as faWalletSolid,
+  faAddressCard as faAddressCardSolid,
+  faKey as faKeySolid,
+  faTriangleExclamation as faTriangleExclamationSolid,
+  faLanguage as faLanguageSolid,
+  faGear as faGearSolid,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart as faHeartRegular,
+  faFileLines as fileLinesRegular,
+} from "@fortawesome/free-regular-svg-icons";
+
+export const ICONS_SOLID = {
+  corazon: faHeartSolid,
+  mando: faGamepadSolid,
+  regalo: faGiftSolid,
+  billetera: faWalletSolid,
+  perfil: faAddressCardSolid,
+  llave: faKeySolid,
+  alerta: faTriangleExclamationSolid,
+  idioma: faLanguageSolid,
+  configuracion: faGearSolid,
+};
+
+export const ICONS_REGULAR = {
+  corazon: faHeartRegular,
+  archivo: fileLinesRegular,
+};
