87be6bf2eb48edaa7fc337db15148f31fd65925a
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-08 00:44:16 -0300

    test(pages): Agrega paginas de prueba

---
+++ src/components/pages/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../atoms/Button";
+
+export default function Test() {
+  return (
+    <>
+      <h1>Test</h1>
+      <div style={{ width: "100%" }}>
+        <Button text={"Hola Mundo!"} />
+      </div>
+    </>
+  );
+}
