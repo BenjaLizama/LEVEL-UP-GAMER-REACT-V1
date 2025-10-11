cc20bcdb3d640825f9c18a580443ab04d4db84d3
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-11 03:34:48 -0300

    feat(NavButtonBar): Implementa mejoras visuales a la navegacion entre paginas

--- src/components/organisms/NavBottomBar.jsx
+++ src/components/organisms/NavBottomBar.jsx
@@ -2,26 +2,31 @@ import { Link } from "react-router-dom";
 import Icon from "../atoms/Icon";
 import { ICONS_SOLID, ICONS_REGULAR } from "../../assets/utils/icons";
 import styles from "../../styles/NavBottomBar.module.css";
+import { useState } from "react";
 
 export default function NavBottomBar() {
+  const [selected, setSelected] = useState(0);
+
+  const links = [
+    { path: "/", icon: ICONS_SOLID.home, label: "Inicio" },
+    { path: "/marketplace", icon: ICONS_SOLID.shop, label: "Marketplace" },
+    { path: "/cart", icon: ICONS_SOLID.cart, label: "Carrito" },
+    { path: "/profile", icon: ICONS_SOLID.usuario, label: "Mi Perfil" },
+  ];
+
   return (
     <nav className={styles.navbar}>
-      <Link to={"/"} className={styles.navButton}>
-        <Icon icon={ICONS_SOLID.home} />
-        <span>Inicio</span>
-      </Link>
-      <Link to={"/marketplace"} className={styles.navButton}>
-        <Icon icon={ICONS_SOLID.shop} />
-        <span>Marketplace</span>
-      </Link>
-      <Link to={"/cart"} className={styles.navButton}>
-        <Icon icon={ICONS_SOLID.cart} />
-        <span>Carrito</span>
-      </Link>
-      <Link to={"/profile"} className={styles.navButton}>
-        <Icon icon={ICONS_SOLID.usuario} />
-        <span>Mi Perfil</span>
-      </Link>
+      {links.map((link, index) => (
+        <Link
+          key={index}
+          to={link.path}
+          className={selected === index ? styles.selected : styles.navButton}
+          onClick={() => setSelected(index)}
+        >
+          <Icon icon={link.icon} />
+          <span>{link.label}</span>
+        </Link>
+      ))}
     </nav>
   );
 }
