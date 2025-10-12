d5d577514b97fe5519d08bb8f81bf64297b901a4
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-12 00:43:34 -0300

    chore(App): Modifica estilos generales de la App e implementa ruta hacia /test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBottomBar from "./components/organisms/NavBottomBar";
 import Marketplace from "./components/pages/Marketplace";
 import Cart from "./components/pages/Cart";
 import Profile from "./components/pages/Profile";
+import Test from "./components/pages/Test";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/marketplace" element={<Marketplace />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
       <NavBottomBar />
     </>
