b59fe84e8108f79f39fa17f8f29f2da22ab05ac3
Lucciano Martinez <lucc.martinez@duocuc.cl>
2025-10-18 13:31:37 -0300

    Agrega el componente Card.jsx (Guías)

--- src/components/molecules/Card.jsx
+++ src/components/molecules/Card.jsx
@@ -3,9 +3,10 @@ import styles from "../../styles/Card.module.css";
 export default function Card({ CardTitle, CardDescription, CardImage }) {
   return (
     <div className={styles.card}>
-      <img className={styles.image} src={CardImage} alt="Imagen de la guia" />
+      <div className={styles.img} style={{ "--img-bg": `url(${CardImage})` }}>
+        <p className={styles.desc}>{CardDescription}</p>
+      </div>
       <h2 className={styles.title}>{CardTitle}</h2>
-      <p className={styles.desc}>{CardDescription}</p>
     </div>
   );
 }
