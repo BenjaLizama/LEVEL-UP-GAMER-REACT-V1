13b70e5aaadfafb3cadf4b89372827c02edea9af
Lucciano Martinez <lucc.martinez@duocuc.cl>
2025-10-18 13:49:28 -0300

    Renombra Card.jsx -> Guia.jsx

--- src/components/molecules/Guia.jsx
+++ src/components/molecules/Guia.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../styles/Card.module.css";
-export default function Card({ CardTitle, CardDescription, CardImage }) {
+export default function Guia({ CardTitle, CardDescription, CardImage }) {
   return (
     <div className={styles.card}>
       <div className={styles.img} style={{ "--img-bg": `url(${CardImage})` }}>
