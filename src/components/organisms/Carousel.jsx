21c1afd6f67f15723f4a26c1ceeedbd194b52ac6
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-18 22:39:02 -0300

    feat: Mejora de componente Carousel.jsx

--- src/components/organisms/Carousel.jsx
+++ src/components/organisms/Carousel.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Carousel.module.css";
 import SimpleIcon from "../atoms/SimpleIcon";
 
-export default function Carousel() {
+export default function Carousel({ listImages = [] }) {
+  const [index, setIndex] = useState(0);
+
+  const next = () => {
+    setIndex((prev) => (prev + 1) % listImages.length);
+  };
+
+  const prev = () => {
+    setIndex((prev) => (prev - 1 + listImages.length) % listImages.length);
+  };
+
   return (
     <div className={styles.container}>
-      <button className={styles.btn}>
-        <SimpleIcon icon={"icono next aqui"} />
-      </button>
-      <div className={styles.carousel}>
-        {/* Debe contener las imagenes dentro de un contenedor slide */}
-      </div>
-      <button className={styles.btn}>
+      <button className={styles.btn} onClick={prev}>
         <SimpleIcon icon={"icono prev aqui"} />
+      </button>
+
+      <div className={styles.carousel}>
+        <img
+          src={listImages[index]}
+          alt={`Imagen ${index + 1}`}
+          className={styles.image}
+        />
+      </div>
+
+      <button className={styles.btn} onClick={next}>
+        <SimpleIcon icon={"icono next aqui"} />
       </button>
     </div>
   );
