ae7ac7ec00f66f7b6090b66c9dc1067d2bb856e5
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-19 14:46:52 -0300

    feat: Implementa componentes a pagina Home (#5)
    * feat: Agrega componentes a pagina Home
    
    * feat: Agrega componentes a pagina Home e implementa Componente SpotLightBorder

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,78 @@
 import React from "react";
-import Button from "../atoms/Button";
+import styles from "../../styles/Home.module.css";
+import Carousel from "../organisms/Carousel";
+import Title from "../atoms/Title";
+import Guia from "../organisms/Guia";
+import Publication from "../organisms/Publication";
+import VoteCard from "../molecules/VoteCard";
+import AboutUs from "../organisms/AboutUs";
+import Footer from "../organisms/Footer";
+import { CAROUSEL_IMAGES } from "../templates/CarouselImages";
+import { GUIAS_INFO } from "../templates/GuiasInfo";
+import { NOTICES_DATA } from "../templates/NoticesData";
+import { VOTE_CARD } from "../templates/VoteCardData";
+import SpotLightBorder from "../atoms/SpotLightBorder";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.first}>
+        <Title text={"Lanzamientos mas esperados"} />
+        <Carousel listImages={CAROUSEL_IMAGES.list} />
+      </div>
+
+      <div className={styles.second}>
+        <Title text={"Guías de videojuegos"} />
+        <Guia
+          CardTitle={GUIAS_INFO.guide1.CardTitle}
+          CardImage={GUIAS_INFO.guide1.CardImage}
+        />
+        <div className={styles.guias}>
+          <Guia
+            CardTitle={GUIAS_INFO.guide2.CardTitle}
+            CardImage={GUIAS_INFO.guide2.CardImage}
+          />
+          <Guia
+            CardTitle={GUIAS_INFO.guide3.CardTitle}
+            CardImage={GUIAS_INFO.guide3.CardImage}
+          />
+          <Guia
+            CardTitle={GUIAS_INFO.guide4.CardTitle}
+            CardImage={GUIAS_INFO.guide4.CardImage}
+          />
+          <Guia
+            CardTitle={GUIAS_INFO.guide5.CardTitle}
+            CardImage={GUIAS_INFO.guide5.CardImage}
+          />
+        </div>
+      </div>
+
+      <div className={styles.darkBackground}>
+        <SpotLightBorder>
+          <div className={styles.third}>
+            <Title text="Últimas noticias" />
+            {Object.values(NOTICES_DATA).map((n, i) => (
+              <Publication key={i} {...n} />
+            ))}
+          </div>
+        </SpotLightBorder>
+        <SpotLightBorder>
+          <div className={styles.fourth}>
+            <div className={styles.top}>
+              <Title text={"Top juegos"} />
+              {Object.values(VOTE_CARD).map((n, i) => (
+                <VoteCard key={i} {...n} />
+              ))}
+            </div>
+          </div>
+        </SpotLightBorder>
+      </div>
+      <div className={styles.fifth}>
+        <AboutUs />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 }
---
+++ src/components/templates/VoteCardData.jsx
@@ -0,0 +1,26 @@
+export const VOTE_CARD = {
+  card1: {
+    gameImage: "../../public/images/Hollow.jpeg",
+    name: "Hollow Knight: Silksong",
+  },
+  card2: {
+    gameImage: "../../public/images/GodOfWar.jpg",
+    name: "God of War: Ragnarok",
+  },
+  card3: {
+    gameImage: "../../public/images/Hell2.jpg",
+    name: "Helldivers 2",
+  },
+  card4: {
+    gameImage: "../../public/images/Mafia.jpg",
+    name: "Mafia: The Old Country",
+  },
+  card5: {
+    gameImage: "../../public/images/GTA.jpg",
+    name: "Grand Theft Auto V",
+  },
+  card6: {
+    gameImage: "../../public/images/Tlou.jpg",
+    name: "The Last Of Us Part II",
+  },
+};
