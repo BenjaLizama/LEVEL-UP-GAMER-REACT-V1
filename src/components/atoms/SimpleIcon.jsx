15ed05f3066a98ff64b095b2b27ede0cb9d36440
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-10-19 23:02:02 -0300

    feat: Implementa parametro boolShadow a SimpleIcon.jsx

--- src/components/atoms/SimpleIcon.jsx
+++ src/components/atoms/SimpleIcon.jsx
@@ -1,10 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../../styles/SimpleIcon.module.css";
 
-export default function SimpleIcon({ icon, size, color }) {
+export default function SimpleIcon({ icon, size, color, boolShadow }) {
+  let active = "";
+
+  if (boolShadow == true) {
+    let active = styles.iconShadow;
+  } else {
+    let active = null;
+  }
   return (
     <FontAwesomeIcon
-      className={styles.iconShadow}
+      className={active}
       icon={icon}
       style={{ fontSize: `${size}rem`, color }}
     />
